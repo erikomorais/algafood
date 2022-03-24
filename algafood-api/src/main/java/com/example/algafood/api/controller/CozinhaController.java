03d297e250f0ad2b04f0b2f5c2014a739881cd8a
eriko <eriko@mppa.mp.br>
2022-03-24 15:06:20 -0300

    atualização da aula 4.23

--- algafood-api/src/main/java/com/example/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/example/algafood/api/controller/CozinhaController.java
@@ -1,20 +1,15 @@
 package com.example.algafood.api.controller;
 
-import java.util.List;
-
 import com.example.algafood.api.model.CozinhasXmlWrapper;
 import com.example.algafood.domain.model.Cozinha;
 import com.example.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cozinhas")
@@ -37,19 +32,19 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-        if (cozinha!=null) {
+        if (cozinha != null) {
             return ResponseEntity.ok(cozinha);
-        }else{
+        } else {
             return ResponseEntity.notFound().build();
         }
-
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
-//
-//        return ResponseEntity
-//                .status(HttpStatus.FOUND)
-//                .headers(headers)
-//                .build();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody Cozinha cozinha) {
+        cozinhaRepository.salvar(cozinha);
+
+      }
+
 
 }
