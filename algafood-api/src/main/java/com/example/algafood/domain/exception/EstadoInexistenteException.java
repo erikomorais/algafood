a547318f94f084d30aec853bc9040c7969860c9f
eriko <eriko@mppa.mp.br>
2022-08-04 13:25:28 -0300

    8.6 refatorando os serviços REST

--- algafood-api/src/main/java/com/example/algafood/domain/exception/EstadoInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package com.example.algafood.domain.exception;
-
-public class EstadoInexistenteException extends RuntimeException {
-    public EstadoInexistenteException(String message) {
-        super(message);
-    }
-   }
