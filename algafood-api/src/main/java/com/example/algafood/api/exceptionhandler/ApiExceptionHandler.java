c522443e56f5fff5eeefb27de7646c0fcd8536ce
Eriko Morais <eriko@mppa.mp.br>
2022-08-07 11:22:04 -0300

    8.16 customizando corpo do ResponseExceptionHandler

--- algafood-api/src/main/java/com/example/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/example/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,11 +3,12 @@ package com.example.algafood.api.exceptionhandler;
 import com.example.algafood.domain.exception.EntidadeEmUsoException;
 import com.example.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.example.algafood.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -15,34 +16,27 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<ApiError> trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e){
-        ApiError error = ApiError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(error);
+    public ResponseEntity<?> trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<ApiError> trataEntidadeEmUsoException(EntidadeEmUsoException e){
-        ApiError error = ApiError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(error);
+    public ResponseEntity<?> trataEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
+
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<ApiError> trataNegocioException(NegocioException e){
-        ApiError error = ApiError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(error);
+    public ResponseEntity<?> trataNegocioException(NegocioException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
-
-
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (body == null) {
+            body = ApiError.builder().dataHora(LocalDateTime.now()).mensagem(status.getReasonPhrase()).build();
+        }else if (body instanceof String){
+            body = ApiError.builder().dataHora(LocalDateTime.now()).mensagem((String) body).build();
+        }
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 }
